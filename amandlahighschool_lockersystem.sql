b0cd00fb4fb435ccdd5e66a523dd6424068bc321
AbriellaM <140449790+AbriellaM@users.noreply.github.com>
2025-11-20 07:22:47 +0200

    Add files via upload

--- amandlahighschool_lockersystem.sql
+++ amandlahighschool_lockersystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2025 at 01:13 AM
+-- Generation Time: Nov 20, 2025 at 01:42 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -81,7 +81,15 @@ INSERT INTO `bookings` (`BookingID`, `StudentSchoolNumber`, `ParentID`, `BookedO
 ('B21', 603268, 6304187924088, '2025-11-17', '2026-04-13', 'Allocated', 2, 'L33-ROW D'),
 ('B22', 612729, 7609273569089, '2025-11-18', '2026-05-11', 'Allocated', NULL, 'L14-ROW B'),
 ('B23', 503146, 9103216894086, '2025-11-19', '2026-02-23', 'Allocated', 2, 'L56-ROW F'),
+('B24', 102349, 9203185496084, '2025-11-19', '2026-01-26', 'Allocated', NULL, 'L10-ROW A\r\n'),
+('B25', 213789, 7405114908087, '2025-11-19', '2026-03-03', 'Waiting', NULL, NULL),
+('B26', 302681, 5911123976088, '2025-11-19', '2026-02-16', 'Waiting', NULL, NULL),
+('B27', 102350, 9203185496084, '2025-11-19', '2026-01-12', 'Allocated', 2, 'L16-ROW B\r\n\r\n'),
+('B28', 416532, 5911123976088, '2025-11-19', '2026-02-16', 'Allocated', 2, 'L3-ROW A\r\n'),
+('B29', 962513, 7405114908087, '2025-11-19', '2026-02-24', 'Allocated', 2, 'L57-ROW F'),
 ('B3', 796234, 8509165792086, '2025-11-06', '2026-01-28', 'Allocated', NULL, NULL),
+('B30', 407682, 8910285473086, '2025-11-19', '2026-04-21', 'Allocated', NULL, 'L77-ROW H'),
+('B31', 407683, 8910285473086, '2025-11-20', '2026-03-02', 'Allocated', 2, 'L34-ROW D'),
 ('B4', 321789, 9905152039080, '2025-11-07', '2026-01-28', 'Allocated', NULL, NULL),
 ('B5', 910983, 4109137427178, '2025-11-07', '2026-01-27', 'Allocated', NULL, NULL),
 ('B6', 589721, 7908165489089, '2025-11-08', '2026-02-09', 'Allocated', NULL, NULL),
@@ -107,17 +115,17 @@ CREATE TABLE `lockers` (
 
 INSERT INTO `lockers` (`LockerID`, `LockerGrade`, `Status`, `StudentSchoolNumber`) VALUES
 ('L1 - ROW A', 8, 'Allocated', 139078),
-('L10-ROW A\r\n', 8, 'Available', NULL),
+('L10-ROW A\r\n', 8, 'Allocated', 102349),
 ('L11-ROW B\r\n', 9, 'Allocated', 321908),
 ('L12-ROW B\r\n', 9, 'Allocated', 811764),
 ('L13-ROW B\r\n', 9, 'Allocated', 239170),
 ('L14-ROW B', 9, 'Allocated', 612729),
 ('L15-ROW B\r\n', 9, 'Allocated', 712908),
-('L16-ROW B\r\n\r\n', 9, 'Available', NULL),
+('L16-ROW B\r\n\r\n', 9, 'Allocated', 102350),
 ('L17-ROW B', 9, 'Available', NULL),
 ('L18-ROW B\r\n', 9, 'Available', NULL),
 ('L19-ROW B\r\n', 9, 'Available', NULL),
-('L2-ROW A', 8, 'Available', 710964),
+('L2-ROW A', 8, 'Allocated', 416532),
 ('L20-ROW B', 9, 'Available', NULL),
 ('L21-ROW C', 9, 'Available', NULL),
 ('L22-ROW C', 9, 'Available', NULL),
@@ -128,12 +136,12 @@ INSERT INTO `lockers` (`LockerID`, `LockerGrade`, `Status`, `StudentSchoolNumber
 ('L27-ROW C', 9, 'Available', NULL),
 ('L28-ROW C', 9, 'Available', NULL),
 ('L29-ROW C', 9, 'Available', NULL),
-('L3-ROW A\r\n', 8, 'Available', NULL),
+('L3-ROW A\r\n', 8, 'Allocated', 416532),
 ('L30-ROW C', 9, 'Available', NULL),
 ('L31-ROW D', 10, 'Allocated', 621908),
 ('L32-ROW D', 10, 'Allocated', 372162),
 ('L33-ROW D', 10, 'Allocated', 603268),
-('L34-ROW D', 10, 'Available', NULL),
+('L34-ROW D', 10, 'Allocated', 407683),
 ('L35-ROW D', 10, 'Available', NULL),
 ('L36-ROW D', 10, 'Available', NULL),
 ('L37-ROW D', 10, 'Available', NULL),
@@ -158,7 +166,7 @@ INSERT INTO `lockers` (`LockerID`, `LockerGrade`, `Status`, `StudentSchoolNumber
 ('L54-ROW F', 11, 'Allocated', 619873),
 ('L55-ROW F', 11, 'Allocated', 192317),
 ('L56-ROW F', 11, 'Allocated', 503146),
-('L57-ROW F', 11, 'Available', NULL),
+('L57-ROW F', 11, 'Allocated', 962513),
 ('L58-ROW F', 11, 'Available', NULL),
 ('L59-ROW F', 11, 'Available', NULL),
 ('L6- ROW A\r\n', 8, 'Available', NULL),
@@ -180,7 +188,7 @@ INSERT INTO `lockers` (`LockerID`, `LockerGrade`, `Status`, `StudentSchoolNumber
 ('L74-ROW G', 11, 'Available', NULL),
 ('L75-ROW H', 12, 'Allocated', 852907),
 ('L76-ROW H', 12, 'Allocated', 419652),
-('L77-ROW H', 12, 'Available', NULL),
+('L77-ROW H', 12, 'Allocated', 407682),
 ('L78-ROW H', 12, 'Available', NULL),
 ('L79-ROW H', 12, 'Available', NULL),
 ('L8-ROW A\r\n', 8, 'Available', NULL),
@@ -217,6 +225,7 @@ INSERT INTO `parents` (`ParentID`, `ParentTitle`, `ParentName`, `ParentSurname`,
 (4109137427178, 'Mr', 'James', 'Thomas', 'abigailmichaels350@gmail.com', '246 Redwood Court', 789012345, 'Pass@6'),
 (5004038523192, 'Ms', 'Linda', 'Anderson', 'abigailmichaels350@gmail.com', '135 Spruce Street', 778901234, 'Pass@7'),
 (5612226663035, 'Mr', 'Robert', 'Wilson', 'abigailmichaels350@gmail.com', '189 Pine Street', 735460310, 'Pass@8'),
+(5911123976088, 'Mr', 'Duresh', 'Reddy', 'abigailmichaels350@gmail.com', '67 Green Street, Norwood', 786541230, 'Pass@39'),
 (6304187924088, 'Mr', 'Jonathan', 'Kramer', 'abigailmichaels350@gmail.com', '789 Spring Street,Broadacres', 786543009, 'Pass@33'),
 (6507115698088, 'Mr', 'Lester', 'James', 'abigailmichaels350@gmail.com', '78 Silverstone Road, Broadacres', 786541290, 'Pass@23'),
 (6703197213084, 'Mr', 'Graham', 'Seale', 'abigailmichaels350@gmail.com', '923 Marine Drive, Summerstrand', 675431290, 'Pass@24'),
@@ -226,6 +235,7 @@ INSERT INTO `parents` (`ParentID`, `ParentTitle`, `ParentName`, `ParentSurname`,
 (7009119979038, 'Mrs', 'Ava', 'Wilson', 'ava.wilson@gmail.com', '911 Birch Street', 764281373, 'P97634^'),
 (7110115033116, 'Mr', 'John', 'Smith', 'john.smith@gmail.com', '123 Maple Street', 821234567, 'P23456@'),
 (7303154126003, 'Dr', 'Ethan', 'Taylor', 'ethan.taylor@gmail.com', '404 Oak Street', 799314784, 'P92365*'),
+(7405114908087, 'Mr', 'Tshepo', 'Majola', 'abigailmichaels350@gmail.com', '569 Worracker Road, Norwood', 785664312, 'Pass@37'),
 (7501236921081, 'Mrs', 'Dianne', 'Beaton', 'abigailmichaels350@gmail.com', '78 Cessna Road, Eldorado Park', 876541230, 'Pass@9'),
 (7608153221084, 'Ms', 'Sameerah', 'Solomons', 'abigailmichaels350@gmail.com', '78 Glenningdale Avenue , Parktown', 786541290, 'Pass@10'),
 (7609273569089, 'Mrs', 'Celine', 'Hart', 'abigailmichaels350@gmail.com', '567 Rhode Drive, Sunninghill', 786541289, 'Pass@34'),
@@ -240,11 +250,13 @@ INSERT INTO `parents` (`ParentID`, `ParentTitle`, `ParentName`, `ParentSurname`,
 (8805123427084, 'Ms', 'Leigh', 'Claasen', 'abigailmichaels350@gmail.com', '78 Marine Drive, Summerstrand', 762348965, 'Pass@20'),
 (8806113921085, 'Ms', 'Lynn', 'Green', 'abigailmichaels350@gmail.com', '678 Bloom Drive , Kilarney', 789065432, 'Pass@30'),
 (8906185498082, 'Mrs', 'Jillene', 'Steele', 'abigailmichaels350@gmail.com', '786 Pratt Street, Broadacres', 765429087, 'Pass@25'),
+(8910285473086, 'Mrs', 'Taahira', 'Abrahams', 'abigailmichaels350@gmail.com', '2010 Marine Drive,Summerstrand', 678907654, 'Pass@40'),
 (9012185498087, 'Mrs', 'Verushka', 'Patel', 'abigailmichaels350@gmail.com', '89 Smythe Avenue,Linden', 765438907, 'Pass@26'),
 (9103216894086, 'Mr', 'Johan', 'Van Wyk', 'abigailmichaels350@gmail.com', '789 Marine Drive,Summerstrand', 675431290, 'Pass@35'),
 (9108286769008, 'Mr', 'Noah', 'Taylor', 'noah.taylor@gmail.com', '566 Ash Street', 787273081, 'P16590%'),
 (9111216984084, 'Mr', 'Lionel', 'Jones', 'lioneljones@gmail.com', '68 Bird Street', 675332890, 'Pass@13'),
 (9122667840156, 'Mr', 'Noah', 'Nkosi', 'noah.nkosi@gmail.com', '353 Oak Street', 763407066, 'P90546&'),
+(9203185496084, 'Mrs', 'Anna', 'Joubert', 'abigailmichaels350@gmail.com', '780 Peck Road,Ormonde', 786528907, 'Pass@36'),
 (9409168497083, 'Mr', 'Ronald', 'Brown', 'abigailmichaels350@gmail.com', '678 Voortrekker Road, Roodepoort', 786541290, 'Pass@31'),
 (9503192107085, 'Mr', 'Areeb', 'Braaf', 'abigailmichaels350@gmail.com', '56 Brown Street,Sandton', 786531468, 'Pass@14'),
 (9503207652085, 'Mrs', 'Fatima', 'Monsoor', 'apadayachy630@gmail.com', '38 Adams Drive', 788229946, 'Pass@15'),
@@ -282,7 +294,12 @@ INSERT INTO `payments` (`PaymentID`, `StudentSchoolNumber`, `BookingID`, `Paymen
 ('P15', 603268, 'B21', 100.00, '2025-11-17', 'Verified', 'proof_B21.pdf'),
 ('P16', 612729, 'B22', 100.00, '2025-11-18', 'Verified', 'proof_B22.pdf'),
 ('P17', 503146, 'B23', 100.00, '2025-11-19', 'Verified', 'proof_B23.pdf'),
+('P18', 416532, 'B28', 100.00, '2025-11-19', 'Verified', 'proof_B28.pdf'),
+('P19', 962513, 'B29', 100.00, '2025-11-19', 'Verified', 'proof_B29.pdf'),
 ('P2', 852907, 'B7', 100.00, '2025-11-08', 'Verified', '1762637095_PROOF OF PAYMENT.pdf'),
+('P20', 407682, 'B30', 100.00, '2025-11-19', 'Verified', 'proof_B30.pdf'),
+('P21', 407682, 'B30', 100.00, '2025-11-19', 'Rejected', 'proof_B30.pdf'),
+('P22', 407683, 'B31', 100.00, '2025-11-20', 'Verified', 'proof_B31.pdf'),
 ('P3', 811764, 'B9', 100.00, '2025-11-14', 'Verified', 'proof_691119fdf2f867.03821743.pdf'),
 ('P4', 239170, 'B11', 100.00, '2025-11-14', 'Verified', 'pay_6916f425a9de37.24149842'),
 ('P5', 621908, 'B12', 100.00, '2025-11-14', 'Verified', 'proof_B12.pdf'),
@@ -310,22 +327,29 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`StudentSchoolNumber`, `StudentName`, `StudentSurname`, `StudentGrade`, `ParentID`) VALUES
+(102349, 'Minke', 'Joubert', 'Grade 8', 9203185496084),
+(102350, 'Kobus', 'Joubert', 'Grade 9', 9203185496084),
 (123456, 'Emma', 'Smith', 'Grade 9', 7110115033116),
 (139078, 'Jehdene', 'Patel', 'Grade 8', 9012185498087),
 (192317, 'Saskia', 'Daniels', 'Grade 11', 2309183217084),
 (200443, 'Ava', 'Anderson', 'Grade 11', 5004038523192),
 (205637, 'Caleb', 'Beaton', 'Grade 9', 7501236921081),
 (207541, 'Ameerah', 'Monsoor', 'Grade 9', 9503207652085),
+(213789, 'Asanda', 'Majola', 'Grade 9', 7405114908087),
 (239170, 'Judah', 'James', 'Grade 9', 6507115698088),
 (248256, 'Liam', 'Nkosi', 'Grade 10', 6711189204048),
 (277788, 'Olivia', 'Nkosi', 'Grade 10', 9122667840156),
 (287868, 'David', 'Nkosi', 'Grade 8', 6806020888197),
+(302681, 'Rajesh', 'Reddy', 'Grade 11', 5911123976088),
 (321789, 'Grayson', 'Petrus', 'Grade 9', 9905152039080),
 (321908, 'Natheerah', 'Solomons', 'Grade 9', 7608153221084),
 (357874, 'Liam', 'Johnson', 'Grade 8', 3018701182456),
 (366289, 'Liam', 'Anderson', 'Grade 9', 4108192658003),
 (372162, 'Mia', 'Green', 'Grade 10', 8806113921085),
 (390412, 'James', 'Wilson', 'Grade 9', 5612226663035),
+(407682, 'Maleekah', 'Abrahams', 'Grade  12', 8910285473086),
+(407683, 'Abdul', 'Abrahams', 'Grade 10', 8910285473086),
+(416532, 'Priyanka', 'Reddy', 'Grade 8', 5911123976088),
 (419652, 'Zahra', 'Dolley', 'Grade 12', 9710116298083),
 (421639, 'Luke', 'Brown', 'Grade 11', 9409168497083),
 (435784, 'Kaylee', 'Jones', 'Grade 9', 9111216984084),
@@ -352,7 +376,8 @@ INSERT INTO `students` (`StudentSchoolNumber`, `StudentName`, `StudentSurname`, 
 (895067, 'Alice', 'Taylor', 'Grade 9', 2020195031968),
 (908741, 'Lemize', 'Braaf', 'Grade 8', 9503192107085),
 (908979, 'Emily', 'Brown', 'Grade 10', 7807275046086),
-(910983, 'Ethan', 'Thomas', 'Grade 10', 4109137427178);
+(910983, 'Ethan', 'Thomas', 'Grade 10', 4109137427178),
+(962513, 'Sipho', 'Majola', '11', 7405114908087);
 
 -- --------------------------------------------------------
 
@@ -385,7 +410,13 @@ INSERT INTO `waitinglist` (`WaitingListID`, `BookingID`, `ParentID`, `LockerID`,
 ('WL15', 'B21', 6304187924088, 'L33-ROW D', 2, 603268, '2025-11-17 17:03:53', 'Allocated'),
 ('WL16', 'B22', 7609273569089, 'L14-ROW B', 2, 612729, '2025-11-18 20:00:37', 'Allocated'),
 ('WL17', 'B23', 9103216894086, 'L56-ROW F', 2, 503146, '2025-11-18 22:23:37', 'Allocated'),
+('WL18', 'B24', 9203185496084, 'L10-ROW A\r\n', 2, 102349, '2025-11-19 07:09:37', 'Allocated'),
+('WL19', 'B27', 9203185496084, 'L16-ROW B\r\n\r\n', 2, 102350, '2025-11-19 11:21:28', 'Allocated'),
 ('WL2', 'B6', 7908165489089, NULL, 2, 589721, '2025-11-08 11:26:47', 'Allocated'),
+('WL20', 'B28', 5911123976088, 'L3-ROW A\r\n', 2, 416532, '2025-11-19 15:42:24', 'Allocated'),
+('WL21', 'B29', 7405114908087, 'L57-ROW F', 2, 962513, '2025-11-19 15:43:00', 'Allocated'),
+('WL22', 'B30', 8910285473086, 'L77-ROW H', 2, 407682, '2025-11-19 21:05:05', 'Allocated'),
+('WL23', 'B31', 8910285473086, 'L34-ROW D', 2, 407683, '2025-11-19 22:59:27', 'Allocated'),
 ('WL3', 'B7', 8105145069088, 'L75-ROW H', 2, 852907, '2025-11-08 18:54:30', 'Allocated'),
 ('WL4', 'B10', 8804185209089, 'L52-ROW F', 2, 652908, '2025-11-10 02:52:56', 'Allocated'),
 ('WL5', 'B11', 6507115698088, 'L13-ROW B\r\n', 2, 239170, '2025-11-14 09:17:40', 'Allocated'),
@@ -474,9 +505,16 @@ ALTER TABLE `lockers`
   ADD CONSTRAINT `fk_lockers_students` FOREIGN KEY (`StudentSchoolNumber`) REFERENCES `students` (`StudentSchoolNumber`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`StudentSchoolNumber`) REFERENCES `students` (`StudentSchoolNumber`);
+
+--
 -- Constraints for table `students`
 --
 ALTER TABLE `students`
+  ADD CONSTRAINT `fk_students_parent` FOREIGN KEY (`ParentID`) REFERENCES `parents` (`ParentID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_students_parents` FOREIGN KEY (`ParentID`) REFERENCES `parents` (`ParentID`) ON DELETE CASCADE,
   ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`ParentID`) REFERENCES `parents` (`ParentID`);
 
